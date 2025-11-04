9343cdfbf5e120d1d3b02ee9207c5c2e25bc1467
Susana Farías <susana.farias.ve@gmail.com>
2025-11-04 01:12:19 -0300

    subiendo tareas clase 1

--- MiInformacion/MiInformacion.java
+++ MiInformacion/MiInformacion.java
@@ -1,5 +1,16 @@
 package MiInformacion;
 
 public class MiInformacion {
-    
+    public static void main(String[] args) {
+        int edad = 35;
+        String nombre = "Susana Farías";
+        String ciudad = "Angol";
+        String region = "IX Región";
+        String pais = "Chile";
+        String hobby = "programar";
+        String docente1 = "Cande";
+        String docente2 = "Vale";
+
+        System.out.println(String.format("Hola, mi nombre es %s. \nTengo %d años de edad. \nSoy de %s, %s, %s. \nMe gusta aprender a %s nuevos lenguajes.\nMis docentes en este nuevo lenguaje son %s y %s. ", nombre, edad, ciudad, region, pais, hobby, docente1, docente2 ));
+    }
 }
--- TiendaOnline/TiendaOnline.java
+++ TiendaOnline/TiendaOnline.java
@@ -1,52 +1,77 @@
 package TiendaOnline;
 
 public class TiendaOnline {
-   public static void main(String[] args) {
-       // VARIABLES DE LA TIENDA
-       // Mensajes de la aplicación
-       String mensajeBienvenida = "Bienvenido a nuestra tienda en línea, ";
-       String mensajeConfirmacion = ", tu pedido ha sido confirmado ";
-       String mensajeRechazo = ", lamentablemente el artículo seleccionado está agotado";
-       String mensajeMostrarTotal = "El total de tu compra es: $";
-      
-       // Variables de productos (agrega las tuyas a continuación)
-       double precioLibro = 15.99;
-       double precioCamiseta = 25.50;
-       double precioPantalon = 30.3;
-       double precioZapatos = 55.5;
-  
-       // Variables de clientes (agrega las tuyas a continuación)
-       String cliente1 = "Ana";
-       String cliente2 = "Alex";
-       String cliente3 = "Miguel";
+    public static void main(String[] args) {
+        // VARIABLES DE LA TIENDA
+        // Mensajes de la aplicación
+        String mensajeBienvenida = "Bienvenido a nuestra tienda en línea, ";
+        String mensajeConfirmacion = ", tu pedido ha sido confirmado ";
+        String mensajeRechazo = ", lamentablemente el artículo seleccionado está agotado";
+        String mensajeMostrarTotal = "El total de tu compra es: $";
 
-  
-       // Estado de pedidos (agrega las tuyas a continuación)
-       boolean pedidoConfirmadoCliente1 = true; //ana
-       boolean pedidoConfirmadoCliente2 = false; //alex
-       boolean pedidoConfirmadoCliente3 = true;  //miguel
-  
-       // INTERACCIÓN DE LA APLICACIÓN
-       System.out.println(mensajeBienvenida + cliente1); // Muestra "Bienvenido a nuestra tienda en línea, Ana"
-       // ** Aquí irán las declaraciones de impresión relacionadas con la interacción del cliente ** //
+        // Variables de productos (agrega las tuyas a continuación)
+        double precioLibro = 15.99;
+        double precioCamiseta = 25.50;
+        double precioPantalon = 30.3;
+        double precioZapatos = 55.5;
 
-       if(pedidoConfirmadoCliente1){
-        System.out.println(cliente1 + mensajeConfirmacion);
-        System.out.println(mensajeMostrarTotal + precioPantalon);
-       }
+        // Variables de clientes (agrega las tuyas a continuación)
+        String cliente1 = "Ana";
+        String cliente2 = "Alex";
+        String cliente3 = "Miguel";
 
-       System.out.println("------------------------------");
+        // Estado de pedidos (agrega las tuyas a continuación)
+        boolean pedidoConfirmadoCliente1 = true; // ana
+        boolean pedidoConfirmadoCliente2 = true; // alex
+        boolean pedidoConfirmadoCliente3 = true; // miguel
 
-double totalMiguel = precioZapatos + precioLibro;
+        // INTERACCIÓN DE LA APLICACIÓN ANA
+        System.out.println(mensajeBienvenida + cliente1); // Muestra "Bienvenido a nuestra tienda en línea, Ana"
 
-System.out.println(mensajeBienvenida + cliente3);
+        if (pedidoConfirmadoCliente1) {
+            System.out.println(cliente1 + mensajeConfirmacion);
+            System.out.println(mensajeMostrarTotal + precioPantalon);
+        } else {
+            System.out.println(cliente1 = mensajeRechazo);
+        }
 
-if(pedidoConfirmadoCliente3){
-    System.out.println(cliente3 + mensajeConfirmacion);
-    System.out.println(mensajeMostrarTotal + totalMiguel);
-}
+        System.out.println("------------------------------");
 
+        // Miguel (pedido con error)
 
+        System.out.println(mensajeBienvenida + cliente3);
 
-   }
+        double totalMiguelIncorrecto = precioCamiseta + precioPantalon;
+
+        if (pedidoConfirmadoCliente3) {
+            System.out.println(cliente3 + mensajeConfirmacion);
+            System.out.println(mensajeMostrarTotal + totalMiguelIncorrecto);
+        } else {
+            System.out.println(cliente1 = mensajeRechazo);
+        }
+
+        System.out.println("------------------------------");
+
+        // Miguel, pedido correcto
+        double totalMiguelCorrecto = precioZapatos = precioLibro;
+        double diferencia = totalMiguelIncorrecto - totalMiguelCorrecto;
+
+        System.out.println("\n" + cliente3 + " ha detectado un cobro incorrecto");
+        System.out.println("pedido correcto: zapatos + libro");
+        System.out.println(mensajeMostrarTotal + totalMiguelCorrecto);
+        System.out.println("Diferencia a reembolsar: $" + diferencia);
+
+        System.out.println("------------------------------");
+        // Alex
+        System.out.println(mensajeBienvenida + cliente2);
+        double totalAlex = (precioZapatos * 2) + precioLibro;
+
+        if (pedidoConfirmadoCliente2) {
+            System.out.println(cliente2 + mensajeConfirmacion);
+            System.out.println(mensajeMostrarTotal + totalAlex);
+        } else {
+            System.out.println(cliente1 = mensajeRechazo);
+        }
+
+    }
 }
